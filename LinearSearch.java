c90e6c43a10cfb2e4fdfabf66bd23d9adefcf2b1
Subhashis Patbandha <100471429+SubhashisPatbandha4@users.noreply.github.com>
2023-10-10 00:01:33 +0530

    Create LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,25 @@
+public class LinearSearch {
+
+    // Function to perform a linear search on an array
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Element found, return its index
+            }
+        }
+        return -1; // Element not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 6, 8, 10, 12, 14, 16 };
+        int target = 10;
+
+        int result = linearSearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found in the array");
+        }
+    }
+}
